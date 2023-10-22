fbc52bd361d4e172cc001583a3cb1b2dd0e1dbaa
Wathanta Aung <wathanta.aung@interns.eonreality.com>
2023-10-22 12:45:57 +0800

    Created Project

---
+++ The Projects Grimoire/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace The_Projects_Grimoire.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
